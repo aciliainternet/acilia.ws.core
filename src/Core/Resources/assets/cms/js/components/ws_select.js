8750c1fa37b7c27a630c28a4a1a3cb3fbb2e7b5e
Jorge Hernández Ríos <jorge.hernandez@acilia.es>
2022-05-26 17:55:14 +0200

    [FC-996] New feature for selects for ajax searching

--- src/Core/Resources/assets/cms/js/components/ws_select.js
+++ src/Core/Resources/assets/cms/js/components/ws_select.js
@@ -1,4 +1,68 @@
+/* eslint-disable no-underscore-dangle */
 import aSelect from '../modules/a_select';
+
+let selectTranslations = null;
+const fetchLookupDelay = 500;
+let fetchLookupTimeout = null;
+const fetchLookupCache = {};
+
+function populateChoices(choices, items) {
+  const toRemove = choices._currentState.items.filter((item) => item.active);
+  const toKeep = items.filter((item) => !toRemove.includes(item.id));
+
+  choices.setChoices(toKeep, 'value', 'label', true);
+}
+
+async function lookup(choices, apiUrl) {
+  // show temporary loading option
+  choices.clearChoices();
+  choices.setChoices([{ value: 0, label: selectTranslations.loading }, 'value', 'label', true]);
+
+  const query = choices.input.value;
+
+  // check if it is cached
+  if (query in fetchLookupCache) {
+    populateChoices(choices, fetchLookupCache[query]);
+  } else {
+    // not cached, let's fetch the API
+    const url = `${apiUrl}?query=${query}`;
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        populateChoices(choices, data);
+      } else {
+        choices.clearData();
+      }
+    } catch (e) {
+      choices.clearData();
+    }
+  }
+}
+
+function setUpLookup(elm, choices) {
+  // update search limit
+  // eslint-disable-next-line no-param-reassign
+  choices.config.searchResultLimit = 9999;
+
+  // reduce select items
+  if (choices._currentState.choices.length > 100) {
+    const slicedChoices = choices._currentState.choices.slice(0, 100);
+    choices.setChoices(slicedChoices, 'value', 'label', true);
+  }
+
+  // trigger API lookup when the user stops typing
+  elm.addEventListener('search', () => {
+    clearTimeout(fetchLookupTimeout);
+    fetchLookupTimeout = setTimeout(() => lookup(choices, elm.dataset.lookup), fetchLookupDelay);
+  });
+}
 
 function init() {
   const { cmsTranslations } = window;
@@ -6,7 +70,7 @@ function init() {
     throw Error('No CMS Translations defined.');
   }
 
-  const selectTranslations = cmsTranslations.ws_cms_components.select;
+  selectTranslations = cmsTranslations.ws_cms_components.select;
   const config = {
     loadingText: selectTranslations.loading,
     noResultsText: selectTranslations.no_results,
@@ -19,7 +83,11 @@ function init() {
   document.querySelectorAll('[data-component="ws_select"]').forEach((elm) => {
     if (!elm.dataset.wsDisable) {
       config.searchEnabled = elm.dataset.search ? elm.dataset.search : false;
-      aSelect(elm, config);
+      const choices = aSelect(elm, config);
+
+      if (elm.dataset.search && elm.dataset.lookup) {
+        setUpLookup(elm, choices);
+      }
     }
   });
 }
