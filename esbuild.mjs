41fa444ddde3bc91f345793b77910fdfc74b07e9
shadowc <info@matiasjose.com>
2022-11-03 15:43:54 -0300

    Fix manifest

--- esbuild.mjs
+++ esbuild.mjs
@@ -15,8 +15,8 @@ function generateManifest(entry) {
 
   const manifestEntry = entry;
   return Object.keys(manifestEntry).reduce((acc, key) => {
-    manifestEntry[key] = manifestEntry[key].replace('src/Core/Resources/assets/cms', '/bundles/wscore/');
-    return { ...acc, [key]: manifestEntry[key] };
+    manifestEntry[key] = manifestEntry[key].replace('src/Core/Resources/public/', '/bundles/wscore/');
+    return { ...acc, [manifestKeysMap[key]]: manifestEntry[key] };
   }, {});
 }
 
