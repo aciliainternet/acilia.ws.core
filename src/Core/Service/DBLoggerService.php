ec77299bf8e05b7718428121383b8f1881618913
Jose Morlet <joseantonio.morlet@sngular.com>
2022-12-28 11:30:29 -0300

    change monolog

--- src/Core/Service/DBLoggerService.php
+++ src/Core/Service/DBLoggerService.php
@@ -4,22 +4,24 @@ namespace WS\Core\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Level;
 use Monolog\Logger;
+use Monolog\LogRecord;
 
 class DBLoggerService extends AbstractProcessingHandler
 {
     public function __construct(
         protected EntityManagerInterface $em,
-        int | string $level = Logger::DEBUG,
+        int | string | Level $level = Logger::DEBUG,
         bool $bubble = true
     ) {
         parent::__construct($level, $bubble);
     }
 
-    protected function write(array $record): void
+    protected function write(LogRecord $log): void
     {
         $conn = $this->em->getConnection();
-
+        $record = $log->toArray();
         $sql = 'INSERT INTO ws_log (log_id, log_channel, log_message, log_level, log_datetime) VALUES (NULL, ?, ?, ?, ?)';
         $stmt = $conn->prepare($sql);
 
