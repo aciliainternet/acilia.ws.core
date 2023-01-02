020a0b3f4363476767780c25e016601156acf27d
shadowc <info@matiasjose.com>
2023-01-02 16:20:11 +0100

    Add missing controller

--- src/Core/Resources/assets/cms/ts/stimulus_bootstrap.ts
+++ src/Core/Resources/assets/cms/ts/stimulus_bootstrap.ts
@@ -2,9 +2,11 @@ import { Application } from '@hotwired/stimulus';
 import WSSelect from './controllers/ws-select_controller';
 import WSSlug from './controllers/ws-slug_controller';
 import ModuleTranslationSidebar from './controllers/module-translation-sidebar_controller';
+import Translation from './controllers/translation_controller';
 
 // TODO: Use lazy loading with webpack or a similar feature
 const stimulus = Application.start();
 stimulus.register('ws-select', WSSelect);
 stimulus.register('ws-slug', WSSlug);
 stimulus.register('module-translation-sidebar', ModuleTranslationSidebar);
+stimulus.register('translation', Translation);
