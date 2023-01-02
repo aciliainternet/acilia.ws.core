f15f4fb04e504bc178c4875e04b64cda9c512330
shadowc <info@matiasjose.com>
2023-01-02 16:35:27 +0100

    Tooltip stimulus controller

--- src/Core/Resources/assets/cms/ts/stimulus_bootstrap.ts
+++ src/Core/Resources/assets/cms/ts/stimulus_bootstrap.ts
@@ -3,6 +3,7 @@ import WSSelect from './controllers/ws-select_controller';
 import WSSlug from './controllers/ws-slug_controller';
 import ModuleTranslationSidebar from './controllers/module-translation-sidebar_controller';
 import Translation from './controllers/translation_controller';
+import WSTooltip from './controllers/ws-tooltip_controller';
 
 // TODO: Use lazy loading with webpack or a similar feature
 const stimulus = Application.start();
@@ -10,3 +11,4 @@ stimulus.register('ws-select', WSSelect);
 stimulus.register('ws-slug', WSSlug);
 stimulus.register('module-translation-sidebar', ModuleTranslationSidebar);
 stimulus.register('translation', Translation);
+stimulus.register('ws-tooltip', WSTooltip);
