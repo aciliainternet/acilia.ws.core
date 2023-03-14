7ae8453ad73718f02562a76682481f8f56f45ce3
shadowc <info@matiasjose.com>
2023-03-14 14:05:33 +0100

    Add open modal handler

--- src/Core/Resources/assets/cms/ts/controllers/ws-asset-image_controller.ts
+++ src/Core/Resources/assets/cms/ts/controllers/ws-asset-image_controller.ts
@@ -3,6 +3,8 @@ import initModalImageSelector from '../../js/components/ws_assets_images/ui_imag
 import { init as initCropper } from '../../js/components/ws_assets_images/ui_cropper';
 import Modal from '../modules/a_modal';
 
+let modal: Modal | null = null;
+
 export default class extends Controller {
   static targets = ['image', 'placeholder', 'uploadNewImage', 'changeImage', 'removeField'];
 
@@ -20,9 +22,23 @@ export default class extends Controller {
       throw Error('No CMS Translations defined.');
     }
 
-    // document.querySelectorAll('.js-open-modal').forEach((element) => {
-    //   element.addEventListener('click', handleBehaviour);
-    // });
+    if (modal === null) {
+      modal = new Modal({
+        autoOpen: false,
+        updateURL: false,
+        initLoad: false,
+        maxWidth: '1200px',
+        closeOnOverlay: true,
+        closeButton: true,
+        identifier: 'image-selector',
+        onClose: () => {
+          const searchInput = document.querySelector<HTMLInputElement>('.js-search-input');
+          if (searchInput) {
+            searchInput.value = '';
+          }
+        },
+      });
+    }
   }
 
   deleteAssetImage() {
@@ -35,36 +51,23 @@ export default class extends Controller {
     this.uploadNewImageTarget.classList.remove('u-hidden');
   }
 
-  openModal() {
-    // const assetImageElement = document.querySelector(
-    //   `#${event.currentTarget.dataset.idAssetComponent}[data-component="ws_cropper"]`,
-    // );
+  openModal(event: MouseEvent & { currentTarget: HTMLElement }) {
+    const assetImageElement = document.querySelector<HTMLElement>(
+      `#${event.currentTarget.dataset.idAssetComponent}[data-component="ws_cropper"]`,
+    );
   
-    // if (assetImageElement.dataset.displayMode === 'list'
-    //   && document.querySelector('.js-image-selector-modal').offsetWidth === 0
-    //   && document.querySelector('.js-image-selector-modal').offsetHeight === 0) {
-    //   event.preventDefault();
-    //   initModalImageSelector(assetImageElement, modal);
-    // } else if (assetImageElement.dataset.displayMode === 'crop') {
-    //   initCropper(assetImageElement, modal);
-    //   assetImageElement.click();
-    // }
-  }
-
-}
-
-const modal = new Modal({
-  autoOpen: false,
-  updateURL: false,
-  initLoad: false,
-  maxWidth: '1200px',
-  closeOnOverlay: true,
-  closeButton: true,
-  identifier: 'image-selector',
-  onClose: () => {
-    const searchInput = document.querySelector<HTMLInputElement>('.js-search-input');
-    if (searchInput) {
-      searchInput.value = '';
+    if (!assetImageElement) {
+      return;
     }
-  },
-});
+
+    if (assetImageElement.dataset.displayMode === 'list'
+      && document.querySelector<HTMLElement>('.js-image-selector-modal')?.offsetWidth === 0
+      && document.querySelector<HTMLElement>('.js-image-selector-modal')?.offsetHeight === 0) {
+      event.preventDefault();
+      initModalImageSelector(assetImageElement, modal);
+    } else if (assetImageElement.dataset.displayMode === 'crop') {
+      initCropper(assetImageElement, modal);
+      assetImageElement.click();
+    }
+  }
+}
--- src/Core/Resources/assets/cms/ts/core.ts
+++ src/Core/Resources/assets/cms/ts/core.ts
@@ -1,5 +1,2 @@
-// Polyfills... because EDGE 15.
-import 'core-js/features/dom-collections/for-each';
-
 // stimulus
 import './stimulus_bootstrap.ts';
