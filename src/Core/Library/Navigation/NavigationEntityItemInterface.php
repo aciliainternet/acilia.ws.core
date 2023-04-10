8ccb68e4a0d6123e719dbd3d8595e184da3c2344
shadowc <info@matiasjose.com>
2023-04-10 15:36:47 +0200

    entity item interface

--- src/Core/Library/Navigation/NavigationEntityItemInterface.php
+++ src/Core/Library/Navigation/NavigationEntityItemInterface.php
@@ -3,4 +3,16 @@
 namespace WS\Core\Library\Navigation;
 
 interface NavigationEntityItemInterface
-{}
+{
+    /**
+     * Retrieves the entity id to be used inside the cms navigation
+     * section
+     */
+    function getId(): int;
+
+    /**
+     * Returns an entity label to be used inside the cms navigation
+     * section
+     */
+    function getLabel(): string;
+}
