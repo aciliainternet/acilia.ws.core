7d69ca0a9477740accdae4c4955858a6e2265a13
Alejandro Glejberman <alejandro@acilia.es>
2023-04-11 14:16:09 -0300

    asset packge in core

--- src/Core/EventListener/AssetPackageListener.php
+++ src/Core/EventListener/AssetPackageListener.php
@@ -5,18 +5,18 @@ namespace WS\Core\EventListener;
 use Symfony\Component\Asset\Package;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use WS\Core\Service\ContextInterface;
 
 #[AsEventListener(event: RequestEvent::class, method: 'setupAssetPackage', priority: 125)]
 class AssetPackageListener
 {
-    protected ParameterBagInterface $parameterBagInterface;
-    protected Packages $packages;
-
     public function __construct(
-        ParameterBagInterface $parameterBagInterface,
-        Packages $packages
+        private ParameterBagInterface $parameterBagInterface,
+        private Packages $packages,
+        private ContextInterface $context
     ) {
         $this->parameterBagInterface = $parameterBagInterface;
         $this->packages = $packages;
@@ -28,7 +28,7 @@ class AssetPackageListener
             return;
         }
 
-        if (!($this->contextService->isCMS() || $this->contextService->isSite())) {
+        if (!($this->context->isCMS() || $this->context->isSite())) {
             return;
         }
 
