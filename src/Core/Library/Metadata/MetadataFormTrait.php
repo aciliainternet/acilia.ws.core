85bf77cd755fb4b575e4a85244cf64305b1b7a36
Alejandro Glejberman <alejandro.glejberman@sngular.com>
2023-10-05 15:35:15 -0300

    metadata required option

--- src/Core/Library/Metadata/MetadataFormTrait.php
+++ src/Core/Library/Metadata/MetadataFormTrait.php
@@ -7,21 +7,23 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 trait MetadataFormTrait
 {
-    protected function addMetadataFields(FormBuilderInterface $builder)
+    protected function addMetadataFields(FormBuilderInterface $builder, bool $required = false)
     {
         $builder
             ->add('metadataTitle', TextType::class, [
                 'label' => 'metadata.metadataTitle.label',
+                'required' => $required,
                 'translation_domain' => 'ws_cms',
             ])
             ->add('metadataDescription', TextType::class, [
                 'label' => 'metadata.metadataDescription.label',
+                'required' => $required,
                 'translation_domain' => 'ws_cms',
             ])
             ->add('metadataKeywords', TextType::class, [
                 'label' => 'metadata.metadataKeywords.label',
+                'required' => $required,
                 'translation_domain' => 'ws_cms',
-            ])
-        ;
+            ]);
     }
 }
