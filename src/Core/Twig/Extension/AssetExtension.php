be82fe4a834f789892d9db406efd9b47bb630b6f
Alejandro Glejberman <alejandro.glejberman@sngular.com>
2023-10-13 16:04:46 -0300

    get file extension

--- src/Core/Twig/Extension/AssetExtension.php
+++ src/Core/Twig/Extension/AssetExtension.php
@@ -4,24 +4,34 @@ namespace WS\Core\Twig\Extension;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+use WS\Core\Entity\AssetFile;
 use WS\Core\Entity\AssetImage;
+use WS\Core\Service\FileService;
 use WS\Core\Service\ImageService;
 
 class AssetExtension extends AbstractExtension
 {
-    public function __construct(protected ImageService $imageService)
-    {
+    public function __construct(
+        protected ImageService $imageService,
+        protected FileService $fileService
+    ) {
     }
 
     public function getFunctions(): array
     {
         return [
             new TwigFunction('asset_get_image', [$this, 'getImage']),
+            new TwigFunction('asset_get_file', [$this, 'getFile']),
         ];
     }
 
     public function getImage(AssetImage $image, string $rendition, ?string $subRendition = null): string
     {
         return $this->imageService->getImageUrl($image, $rendition, $subRendition);
+    }
+
+    public function getFile(AssetFile $file): string
+    {
+        return $this->fileService->getFileUrl($file);
     }
 }
