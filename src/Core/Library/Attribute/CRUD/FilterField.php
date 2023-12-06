b95d88f61ef4c0ef56623eb00dd998ea24d2a9f7
Alejandro Glejberman <alejandro.glejberman@sngular.com>
2023-12-06 16:07:20 -0300

    extend crud sort options

--- src/Core/Library/Attribute/CRUD/FilterField.php
+++ src/Core/Library/Attribute/CRUD/FilterField.php
@@ -5,8 +5,7 @@ namespace WS\Core\Library\Attribute\CRUD;
 #[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD)]
 final class FilterField
 {
-    public function __construct(
-        public ?string $name = null,
-    ){
+    public function __construct(public ?string $name = null)
+    {
     }
 }
