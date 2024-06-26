80c174d56d5eaada4ccfcb2cd6c6e79b2bc00b64
Alejandro Glejberman <alejandro.glejberman@sngular.com>
2024-06-26 16:07:20 -0300

    upgrade date picker

--- src/Core/Resources/assets/cms/ts/stimulus_bootstrap.ts
+++ src/Core/Resources/assets/cms/ts/stimulus_bootstrap.ts
@@ -1,8 +1,9 @@
 import { Application } from '@hotwired/stimulus';
 import WSSlug from './controllers/ws-slug_controller';
 import WSSelect from './controllers/ws-select_controller';
+import WSDatePicker from './controllers/ws-date-picker_controller';
 
 const stimulus = Application.start();
 stimulus.register('ws-slug', WSSlug);
 stimulus.register('ws-select', WSSelect);
-
+stimulus.register('ws-datepicker', WSDatePicker);
