db75298c7e94e42629b289791e28320e71416fb0
Alejandro Glejberman <alejandro.glejberman@sngular.com>
2025-12-02 08:52:21 -0300

    update ws slug controller

--- src/Core/Resources/assets/cms/ts/controllers/ws-slug_controller.ts
+++ src/Core/Resources/assets/cms/ts/controllers/ws-slug_controller.ts
@@ -1,13 +1,22 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+  declare private regex: string|RegExp
+  declare regexValue: string;
+
+  static values = {
+    regex: String
+  }
+
   connect() {
     this.element.addEventListener('keyup', this.sanitizeInput);
     this.element.addEventListener('keydown', this.validateInput);
+
+    this.regex = this.regexValue || '^[A-Za-z0-9 _-]*$';
   }
 
   validateInput(event: Event) {
-    const regex = new RegExp('^[A-Za-z0-9 _-]*$');
+    const regex = new RegExp(this.regex);
 
     if (!regex.test((event as KeyboardEvent).key)) {
       event.preventDefault();
